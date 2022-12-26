e01629f5c357eafeac45bd9e1b186f8d9d760ef6
Ayush <ayushchopra0@gmail.com>
2022-12-26 21:15:07 +0530

    added user repository functions

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ const bodyParser = require('body-parser');
 const apiRoutes = require('./routes/index'); 
 const {PORT} = require('./config/serverConfig');
 const db = require('./models/index');
+const UserRepository = require('./repository/user-repository');
 
 const app = express();
 
@@ -13,11 +14,13 @@ const prepareAndStartServer = ()=>{
 
     app.use('/api/', apiRoutes);
 
-    app.listen(PORT ,()=>{
+    app.listen(PORT ,async ()=>{
+
         console.log(`Server Started on Port : ${PORT}`);
-        if(process.env.SYNC_DB){
-            db.sequelize.sync({alter:false});
-        }
+        
+            if(process.env.SYNC_DB){
+                db.sequelize.sync({alter:false});
+            }
     })
 }
 
